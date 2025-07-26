c0e81a3fe6d1f71acbcd4d03f845bd4586ca3ef5
Rory Maher <rory.maher@vocus.com.au>
2025-07-26 11:23:06 +1000

    buildinfo

--- app/frontend/src/buildInfo.ts
+++ app/frontend/src/buildInfo.ts
@@ -1,3 +1,3 @@
 // This file contains build information that will be updated during the build process
-export const BUILD_VERSION = "1.7.523";
+export const BUILD_VERSION = "2.0.0";
 export const BUILD_TIME = new Date().toISOString();
